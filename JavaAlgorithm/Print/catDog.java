d38ce1054a95c203c8de3d9d03a2dbee9952e15d
chaeeun <1005honeybadger@gmail.com>
2022-02-11 14:31:13 +0900

    baekjoon 10171, 10172 고양이, 개

---
+++ JavaAlgorithm/Print/catDog.java
@@ -0,0 +1,18 @@
+public class catDog {
+    public static void main(String[] args) {
+        // 10171 고양이
+        // \를 출력하고 싶으면 \\두번 사용
+        System.out.println("\\    /\\"); 
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+
+        // 10172 개
+        System.out.println("|\\_/|"); 
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}

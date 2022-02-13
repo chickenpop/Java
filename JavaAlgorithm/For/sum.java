f3acac1c8179ac667383206a5f99c832418702cc
chaeeun <1005honeybadger@gmail.com>
2022-02-13 22:47:53 +0900

    BOJ 8393 합

---
+++ JavaAlgorithm/For/sum.java
@@ -0,0 +1,18 @@
+//8393 합
+
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int sum = 0;            //합을 저장할 변수
+        int n = in.nextInt();   //n
+        //1~n까지의 합
+        for(int i = 1; i <= n;i++){
+            sum += i;
+        }
+        System.out.println(sum);
+        in.close();
+    }
+}

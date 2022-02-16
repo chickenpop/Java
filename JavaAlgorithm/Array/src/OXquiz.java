1e3335d196c6925578b31c88a673748d0cf18fac
chaeeun <1005honeybadger@gmail.com>
2022-02-16 23:46:30 +0900

    BOJ 8958 OX퀴즈

--- JavaAlgorithm/Array/src/OXquiz.java
+++ JavaAlgorithm/Array/src/OXquiz.java
@@ -9,6 +9,7 @@ public class OXquiz {
         int n = 0; // 횟수
         n = in.nextInt();                           // 테스트 케이스 반복 횟수
         String oxStr = "";
+        String o = "O";
         for(int i = 0; i < n ; i++){
             int sum = 0;                            // 점수 합
             int cnt = 1;                            // 현재 점수
@@ -16,16 +17,16 @@ public class OXquiz {
             String oxStrArr[] = oxStr.split("");    // oxStr 문자 한개씩 배열에 넣기
 
             for(int k = 0; k < oxStrArr.length - 1; k++){
-                if(oxStrArr[k] == "O"){
+                if(oxStrArr[k].equals(o)){
                     sum += cnt;
                 }
-                if(oxStrArr[k] == oxStrArr[k+1] && oxStrArr[k] == "O"){   // 연속되는 문자가 "O"로 같으면 +1
+                if(oxStrArr[k].equals(oxStrArr[k+1]) && oxStrArr[k].equals(o)){   // 연속되는 문자가 "O"로 같으면 +1
                     cnt += 1;
                 }else {
                     cnt = 1;
                 }
             }
-            if(oxStrArr[oxStrArr.length-1] == "O") sum += cnt;
+            if(oxStrArr[oxStrArr.length-1].equals(o)) sum += cnt;
             System.out.println(sum);
         }
 

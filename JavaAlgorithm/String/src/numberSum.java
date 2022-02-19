1495bd43c5f1280260ba16d8b976b8f2c2299187
chaeeun <1005honeybadger@gmail.com>
2022-02-19 22:36:59 +0900

    BOJ 11720 숫자의 합

---
+++ JavaAlgorithm/String/src/numberSum.java
@@ -0,0 +1,22 @@
+// 11720 숫자의 합
+import java.util.Scanner;
+
+public class numberSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // 입력
+        int n = in.nextInt();           // 숫자의 개수
+        String number = in.next();      // 공백없이 숫자 입력
+        int sum = 0;                    // 숫자의 합
+
+        // 숫자의 합 구하기
+        for(int i = 0; i < n; i++){
+            sum += (int)number.charAt(i) - '0';
+        }
+        
+        // 출력
+        System.out.println(sum);
+        in.close();
+    }    
+}

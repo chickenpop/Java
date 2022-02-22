d66eb89265f0fb85f5632ebda974312109e5d142
chaeeun <1005honeybadger@gmail.com>
2022-02-22 22:40:14 +0900

    codeup 1106 int의 범위

---
+++ JavaBasic/codeup/print/src/intRange.java
@@ -0,0 +1,9 @@
+// 1106 int의 범위
+
+public class intRange {
+    public static void main(String[] args) {
+        int a = -2147483648;
+        int b = 2147483647;
+        System.out.printf("%d %d", a, b);
+    }
+}

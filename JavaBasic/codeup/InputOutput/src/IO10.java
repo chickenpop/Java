92e8b9b3683c4b623c758c33e6eabdefcf4b31c6
chaeeun <1005honeybadger@gmail.com>
2022-02-25 22:13:47 +0900

    codeup 1020 다른 방법 [기초-입출력] 주민번호 입력받아 형태 바꿔 출력하기

--- JavaBasic/codeup/InputOutput/src/IO10.java
+++ JavaBasic/codeup/InputOutput/src/IO10.java
@@ -6,12 +6,18 @@ public class IO10 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
-        String humanId[] = in.next().split("-");
+        // String humanId[] = in.next().split("-");
         
-        for(int i = 0; i < humanId.length; i++){
-            System.out.print(humanId[i]);
-        }
+        // for(int i = 0; i < humanId.length; i++){
+        //     System.out.print(humanId[i]);
+        // }
 
+        // 방법 2
+        String humancode = in.nextLine();
+        String humanId[] = humancode.split("\\-");
+
+        System.out.print(String.valueOf(humanId[0]));
+        System.out.print(String.valueOf(humanId[1]));
         in.close();
     }
 }

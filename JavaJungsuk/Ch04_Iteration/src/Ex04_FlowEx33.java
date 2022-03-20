43530650e8d0e2c0242c56b50e9a259e4aa63dfc
chickenpop <1005honeybadger@gmail.com>
2022-03-20 22:32:21 +0900

    ch04 반복문 이름붙은반복문 예제 4-33 Loop - docs

--- JavaJungsuk/Ch04_Iteration/src/Ex04_FlowEx33.java
+++ JavaJungsuk/Ch04_Iteration/src/Ex04_FlowEx33.java
@@ -7,10 +7,10 @@ public class Ex04_FlowEx33 {
             for(int j=1; j<=9; j++){
 
                 if(j==5)
-                    break Loop1;     // Loop1를 탈출 2단 4까지만 출력
+                    break Loop1;     // Loop1를 탈출 2단 4까지만 출력 후 탈출
                     // break;           // j가 5면 가장 가까운 for 탈출
-                    // continue Loop1;  // 모든 단 5이후로 생략
-                    // continue;        // 모든 단 5만 생략
+                    // continue Loop1;  // 모든 단 5이후로 출력되지 않음
+                    // continue;        // 모든 단 5만 출력되지 않음
                 System.out.printf("%d * %d = %d\n", i, j, i*j);
 
             } // end of for i

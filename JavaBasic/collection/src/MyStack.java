91f3f8b7d62fe7e15d0ec4a97344d43f865ca897
chaeeun <1005honeybadger@gmail.com>
2022-04-02 19:47:04 +0900

    컬랙션 stack 과제 - 2번 stack 메소드 주석 추가

--- JavaBasic/collection/src/MyStack.java
+++ JavaBasic/collection/src/MyStack.java
@@ -7,6 +7,7 @@ public class MyStack {
 		this.index = 0;
 	}
 	
+	// 값을 순차적으로 추가함
 	public boolean push(String value) {
 		
 		init();
@@ -20,6 +21,7 @@ public class MyStack {
 		return true;
 	}
 	
+	// 배열 범위 이상의 길이인지 확인
 	private boolean checkLength() {
 		
 		if(this.index == this.list.length) {
@@ -29,12 +31,14 @@ public class MyStack {
 		return false;
 	}
 
+	// 배열 생성
 	private void init() {
 		if(this.index == 0) {
 			list = new String[4];
 		}
 	}
 	
+	// 배열 2배로 생성 및 교체
 	private void doubleStack() {
 		if(this.index == list.length) {
 			String[] temp = new String[this.list.length * 2];
@@ -45,6 +49,7 @@ public class MyStack {
     	}
 	}
 	
+	// 값을 순차적으로 가져옴
 	public String pop() {
 		this.index--;
 		String tmp = this.list[this.index];
@@ -52,6 +57,7 @@ public class MyStack {
 		return tmp;
 	}
 	
+	// 요소의 개수를 반환함
 	public int size() {
 	   int cnt = 0;
 	   for(String i : this.list) {
@@ -61,6 +67,7 @@ public class MyStack {
 	   return cnt;
 	}
 	
+	// 이번에 가져올 값을 확인
 	public String peek() {
 		this.index--;
 		String tmp = this.list[this.index];
@@ -68,6 +75,7 @@ public class MyStack {
 		return tmp;
 	}
 	
+	// 배열의 요소의 수만큼 배열의 길이를 줄임
 	public void trimToSize(){
 		String[] temp = new String[this.list.length];
 		for(int i=0; i<this.list.length; i++) {
@@ -77,6 +85,7 @@ public class MyStack {
 		
 	}
 	
+	// 배열의 모든 요소 삭제함
 	public void clear() {
 		this.list = new String[4];
 		this.index = 0;

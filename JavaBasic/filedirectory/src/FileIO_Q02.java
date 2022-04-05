928e1cb9dee25842c230d172c9a85e30abeed86c
chaeeun <1005honeybadger@gmail.com>
2022-04-05 21:03:40 +0900

    파일입출력 변경 과제 - 2번 숫자 바꾸기, 아라비아 숫자 > 한글 숫자로

---
+++ JavaBasic/filedirectory/src/FileIO_Q02.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class FileIO_Q02 {
+
+	public static void main(String[] args) {
+
+		// 2번 숫자 바꾸기
+		// 0 → 영, 1 → 일 ... 9 → 구
+		// 숫자.dat
+		try {
+			String path = "C:\\class\\java\\questionFileIO\\숫자.dat";
+			BufferedReader reader = new BufferedReader(new FileReader(path));
+		
+			String line = null;
+			
+			while((line = reader.readLine()) != null) {
+				
+				line = numberToString(line);
+				System.out.println(line);
+			}
+			
+			reader.close();
+			System.out.println("변환 후 다른 이름으로 저장하였습니다.");
+		} catch (Exception e) {
+			System.out.println("Q02.main");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private static String numberToString(String line) {
+		line = line.replace("0", "영");
+		line = line.replace("1", "일");
+		line = line.replace("2", "이");
+		line = line.replace("3", "삼");
+		line = line.replace("4", "사");
+		line = line.replace("5", "오");
+		line = line.replace("6", "육");
+		line = line.replace("7", "칠");
+		line = line.replace("8", "팔");
+		line = line.replace("9", "구");
+		return line;
+	}
+	
+
+}

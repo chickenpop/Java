235ec216895ff3133bdf145bb53d10087aa9f0d5
chaeeun <1005honeybadger@gmail.com>
2022-04-05 21:03:10 +0900

    파일입출력 변경 과제 - 1번 이름 바꾸기, 특정 문자 변경

---
+++ JavaBasic/filedirectory/src/FileIO_Q01.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class FileIO_Q01 {
+
+	public static void main(String[] args) {
+		
+		// 1번 이름 바꾸기
+		
+		// 파일 내에서 '유재석' > '메뚜기'로 변환
+		// 파일 > 이름수정.dat
+		
+		
+		try {
+			String path = "C:\\class\\java\\questionFileIO\\이름수정.dat";
+			
+			BufferedReader reader = new BufferedReader(new FileReader(path));
+			
+			String line = null;
+			while((line = reader.readLine()) != null) {
+				System.out.println(line.replace("유재석", "메뚜기"));
+			}
+			
+			reader.close();
+			
+			System.out.println("변환 후 다른 이름으로 저장하였습니다.");
+		} catch (Exception e) {
+			System.out.println("Q01.main");
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
